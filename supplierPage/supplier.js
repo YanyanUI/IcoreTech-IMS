44615d096602a3ca826001f987e6d10fbb5d8278
YanyanUI <rheyyanjulestanawan@gmail.com>
2026-03-13 22:55:07 +0800

    Adding Favicon and designing boxes

---
+++ supplierPage/supplier.js
@@ -0,0 +1,19 @@
+const addSupplier = document.getElementById("addSupplier-btn");
+const closeBtn = document.getElementById("close-btn");
+const popUp = document.getElementById("PopUp-form");
+const overlay = document.getElementById("overlay");
+
+addSupplier.addEventListener('click', () => {
+    popUp.style.display = "block";
+    popUp.style.position = "absolute";
+    overlay.style.display = "block";
+});
+closeBtn.addEventListener('click', () => {
+    popUp.style.display = "none";
+    overlay.style.display = "none";
+})
+
+overlay.addEventListener('click', () => {
+  popUp.style.display = "none";
+  overlay.style.display = "none";
+});
